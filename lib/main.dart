01b211acb00fd7875d06e488b1309a303d8b8494
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2023-09-06 11:37:39 -0300

    feat: adicionado arquivo para concentração de rotas, e criação e navegação para a tela de detalhes do produto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,6 +25,9 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Lato',
       ),
       home: ProductsOverviewPage(),
+      routes: {
+        AppRoutes.product_detail: (ctx) => const ProductDetailPage(),
+      },
     );
   }
 }
