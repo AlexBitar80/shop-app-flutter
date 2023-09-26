2f9d481fb7bf781b817741294651daf2231982e8
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2023-09-26 12:16:12 -0300

    feat: Adicionado componente para a tela de detalhes do produto

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:shop/models/product.dart';
 
 class ProductDetailPage extends StatelessWidget {
@@ -10,9 +13,53 @@ class ProductDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final Product product =
         ModalRoute.of(context)?.settings.arguments as Product;
+
+    final formatCurrency = NumberFormat.simpleCurrency(
+      locale: 'pt_BR',
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(product.name),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300.0,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              formatCurrency.format(
+                product.price,
+              ),
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20.0,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
